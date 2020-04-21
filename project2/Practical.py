9873eb84df11a88a956ef2fb14f49296bad728e5
Your Name <a.almualim@hotmail.com>
2020-04-22 02:01:33 +0300

    finsh update

--- project2/Practical.py
+++ project2/Practical.py
@@ -1,4 +1,5 @@
-name= input("Enter your Name : ")
-age = input("Enter your Age : ")
-print ("Hello " +name+", your Age :  "+age )
+num1= input("Enter your num 1 : ")
+num2 = input("Enter your num 2 : ")
+sum= int(num1)+ float (num2) #int= "عدد صحيح" , float="عدد كسري"
+print (sum )
 
