96b4876c8f28c10ff4c62fb3785e892bab7494a1
Your Name <a.almualim@hotmail.com>
2020-04-22 00:46:09 +0300

    update project1

---
+++ project1/variables.py
@@ -0,0 +1,13 @@
+name = "Anas SAEED AWADH"
+age  ="30"
+bron ="Yemen"
+live ="Saudi Arabia"
+print("My name is "+name.lower())   # lower()=اداه تجعل الاحرف صغسرة
+print("My name is "+name.upper())   # upper()=اداه تجعل الاحرف كبيره
+print("I am "+age+" yers old")
+print("I am from "+bron)
+print("and i leve in "+live)
+print(len(live))                    # len() = خاصيه تاخد طول المتغير
+print(live[2])                      #varbal[2]=خاصيه تاخد ماهو الشي الموجود في المحور رقم 2
+print(live.index("i"))              #varbal.index("") = خاصيه كم ترتيب الحرف المذكور
+print(name.replace("Anas","Osomo")) # vabal.replace()= خاصيه استبدال كلمه بكلمه اخرى
